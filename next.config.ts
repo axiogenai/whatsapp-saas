b8828377610e2b3143b183fec79e3a56b62e8659
axiogen <adityaminchekarfx@gmail.com>
2026-09-08 08:28:51 +0530

    fix: remove mismatched package-lock.json and clean next.config.ts for Vercel

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,5 @@
-import type { NextConfig } from "next";
-import path from "path";
+﻿import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  outputFileTracingRoot: path.join(__dirname),
-  devIndicators: false,
-};
+const nextConfig: NextConfig = {};
 
 export default nextConfig;
